992e5073edf721c2ec5b90b9c235807fb5ee7fa7
Pierre <iwaongit@gmail.com>
2021-06-28 16:44:40 +0200

    feat: :sparkles: add metadata for discord etc...

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,15 @@ export default {
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' }
+            { hid: 'description', name: 'description', content: '' },
+            { property: 'og:type', content: 'website' },
+            { property: 'og:site_name', content: 'quandctropctropi.co' },
+            { property: 'og:title', content: 'quand c trop c tropico' },
+            { property: 'og:description', content: 'non franchement là ça devient trop' },
+            { property: 'og:image', content: 'https://quandctropctropi.co/encore.gif' },
+            { name: 'twitter:site', content: 'quandctropctropi.co' },
+            { name: 'twitter:creator', content: '@iwouah' },
+            { name: 'theme-color', content: '#faecc3' }
         ],
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
